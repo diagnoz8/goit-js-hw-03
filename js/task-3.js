70a5a0646084a1d92afe8929a751063cee7f6fde
Di <diana8burkut@gmail.com>
2024-04-16 21:26:49 +0200

    task3 done

---
+++ js/task-3.js
@@ -0,0 +1,17 @@
+function filterArray(numbers, value) {
+    let smth = [];
+    for (let index = 0; index < numbers.length; index++) {
+        if (numbers[index] > value) {
+           smth.push(numbers[index]);
+        }
+    }
+            return smth;
+
+}
+
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
